7481f46b5d8055c0d8ea6a63351c9069ee895e4a
Asif <asifsaascraft@gmail.com>
2026-01-22 11:57:20 +0530

    Update webinar and course

--- utils/generateTokens.js
+++ utils/generateTokens.js
@@ -5,13 +5,13 @@ export const generateTokens = (id, role) => {
   const accessToken = jwt.sign(
     { id, role, type: 'access' },
     process.env.JWT_SECRET,
-    { expiresIn: '15m' }
+    { expiresIn: '1m' }
   )
 
   const refreshToken = jwt.sign(
     { id, role, type: 'refresh' },
     process.env.JWT_REFRESH_SECRET,
-    { expiresIn: '30d' }
+    { expiresIn: '5m' }
   )
 
   return { accessToken, refreshToken }
